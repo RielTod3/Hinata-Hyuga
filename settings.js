31cd54b70fd6cca4aa294e7045499545fff6e26b
RielTod3 <106985154+RielTod3@users.noreply.github.com>
2022-06-07 07:12:32 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = '𝐎𝐖𝐍𝐄𝐑𝐍𝐘𝐀 𝐆𝐀𝐍𝐓𝐄𝐍𝐆𓆉︎' // Ganti Jadi Namamu
+  global.owner = ['6282146218274','6282146218274','6283893377366'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6282146218274' // Owner Utama Istilahnya
+  global.botname = '𝗥𝗜𝗘𝗟𝗕𝗢𝗧𝗭-𝗠𝗱' // Ganti Jadi Nama Botmu
+  global.packname = '𝚁𝙸𝙴𝙻-𝙼𝙳 ⌫' // Sticker Weem
+  global.author = '𝑩𝒚 𝑹𝒊𝒆𝒍𝒈𝒂𝒏𝒔' // Sticker Weem
+  global.webme = 'https://api-rielbotz.herokuapp.com' // Ganti Sama Web Mu
+  global.linkgroup = 'https://tinyurl.com/y5ck3k4u' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'Gada yt banh🐤' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com/Rieltod' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/gada bnh🐤' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -51,7 +51,7 @@
 
   // Edit Sesuai Kemauan Mu
   global.rpg = {
-  limitEnd: 'Limit Anda Habis\n\n📝 Note : Limit Akan Di Reset Setiap Jam 21:00',
+  limitEnd: 'Limit Anda Habis\n\n📝 Note : Limit Akan Di Reset Setiap Jam 21.00',
   }
 
   // Edit Sesuai Kemauanmu
@@ -63,7 +63,7 @@
   private: '_Khusus Private Chat_',
   bot: '_Khusus Pengguna Nomer Bot_',
   wait: '_Sabar Sedang Di Proses ..._',
-  done: '_Done Ngab 🤗_',
+  done: '_𝐃𝐨𝐧𝐞 𝐧𝐠𝐚𝐛 🎧_',
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
